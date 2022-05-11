ffd066463bf39671cd0149103c6a27739190df0b
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-11 13:15:20 +0200

    create the decorator classes

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-class Person
-  attr_accessor :name, age
+require './nameable.rb'
+
+class Person < Nameable
+  attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,11 +11,11 @@ class Person
     @id = rand(1..100)
   end
 
-  private :of_age?
-
+  
   def of_age?
     @age >= 18
   end
+  private :of_age?
 
   def can_use_services?
     if of_age? || @parent_permission
@@ -22,4 +24,41 @@ class Person
       false
     end
   end
+
+  def correct_name
+    @name
+  end
+
 end
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name[0..9] if @nameable.correct_name.length >= 10
+  end
+end
+
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson.correct_name

74f3727492f398778c1e3cf1fab9ed01ceb67846
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-16 23:42:58 +0200

    Make the app class has less responsabilities

--- app.rb
+++ app.rb
@@ -5,8 +5,10 @@ require './nameable'
 require './book'
 require './rental'
 require './classroom'
+require './create_person'
 
 class App
+  include CreatePerson
   attr_reader :books, :persons, :rentals
 
   def initialize
@@ -32,35 +34,6 @@ class App
       @persons.each_with_index do |person, index|
         puts "#{index}) [#{person.class}], Name: #{person.name} ID: #{person.id}, Age: #{person.age} "
       end
-    end
-  end
-
-  def create_person
-    puts 'Do you want to create a student (1) or a teacher (2)? [input the number]'
-    selected_person = gets.chomp
-    case selected_person
-    when '1'
-      puts 'Age: '
-      age = gets.chomp
-      puts 'Name: '
-      name = gets.chomp
-      puts 'Has parent permission [Y/N]: '
-      parent_permission = gets.chomp
-      puts 'Classroom: '
-      classroom = gets.chomp
-      @persons << Student.new(age, name, parent_permission, classroom)
-      puts 'Person Created succesfully'
-    when '2'
-      puts 'Age: '
-      age = gets.chomp
-      puts 'Name: '
-      name = gets.chomp
-      puts 'Specialization: '
-      specialization = gets.chomp
-      @persons << Teacher.new(age, name, specialization)
-      puts 'Person Created succesfully'
-    else
-      puts 'Invalid selection'
     end
   end
 
@@ -99,44 +72,5 @@ class App
     end
   end
 
-  def pick_option(operation)
-    case operation
-    when '1'
-      list_books
-    when '2'
-      list_persons
-    when '3'
-      create_person
-    when '4'
-      create_book
-    when '5'
-      create_rental
-    when '6'
-      list_rentals
-    end
-    print_options
-  end
 
-  def print_options
-    puts ''
-    puts 'WELCOME TO SCHOOL LIBRARY APP!'
-    puts ''
-    puts 'Please choose an operation from the following options'
-    puts '1  →  List all books'
-    puts '2  →  List all people'
-    puts '3  →  Create a person'
-    puts '4  →  Create a book'
-    puts '5  →  Create a rental'
-    puts '6  →  List all rentals for a given person id'
-    puts '7  →  Exit'
-    operation = gets.chomp
-    exit if operation == '7'
-
-    pick_option(operation)
-  end
-
-  def run
-    option = print_options
-    pick_option(option)
-  end
 end

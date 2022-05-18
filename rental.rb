47e45477bee7d25e9f98af8e8746da59245d780e
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-19 00:26:29 +0200

    Add function to rentals class

--- rental.rb
+++ rental.rb
@@ -10,4 +10,8 @@ class Rental
     @book = book
     book.rental << self
   end
+
+  def attrs
+    instance_variables.map { |ivar| [ ivar.to_s.sub('@', ''), instance_variable_get(ivar)] }.to_h
+  end
 end

652e32353e5450984e6deb34731d11c5c807afce
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-19 00:27:44 +0200

    preserve the data to persons

--- create_person.rb
+++ create_person.rb
@@ -1,6 +1,10 @@
 module CreatePerson
   def create_person
     puts 'Do you want to create a student (1) or a teacher (2)? [input the number]'
+
+    json = File.read('people.json')
+    people = File.zero?('people.json') ? [] : JSON.parse(json)
+
     selected_person = gets.chomp
     case selected_person
     when '1'
@@ -10,18 +14,30 @@ module CreatePerson
       name = gets.chomp
       puts 'Has parent permission [Y/N]: '
       parent_permission = gets.chomp
-      puts 'Classroom: '
-      classroom = gets.chomp
-      @persons << Student.new(age, name, parent_permission, classroom)
+      puts 'Student id: '
+      id = gets.chomp
+      student = Student.new(age, name, id, parent_permission)
+      people << student.attrs
+      File.open("people.json", "w") do |f|
+        f.write(JSON.pretty_generate(people))
+      end
+      @persons << student
       puts 'Person Created succesfully'
     when '2'
       puts 'Age: '
       age = gets.chomp
       puts 'Name: '
       name = gets.chomp
+      puts 'ID: '
+      id = gets.chomp
       puts 'Specialization: '
       specialization = gets.chomp
-      @persons << Teacher.new(age, name, specialization)
+      teacher = Teacher.new(age, name, id, specialization)
+      people << teacher.attrs
+      File.open("people.json", "w") do |f|
+        f.write(JSON.pretty_generate(people))
+      end
+      @persons << teacher
       puts 'Person Created succesfully'
     else
       puts 'Invalid selection'
